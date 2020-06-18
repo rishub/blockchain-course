71969c09ce0ca6abbb9e88e821001e48463432cb
Rishub Kumar <rishub@gmail.com>
2020-06-18 16:35:25 -0700

    casing

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -5,7 +5,7 @@ import MDXRenderer from 'gatsby-plugin-mdx/mdx-renderer';
 
 import { Layout, Link } from '$components';
 import NextPrevious from '../components/NextPrevious';
-import Authors from '../components/Authors';
+import Authors from '../components/authors';
 import config from '../../config';
 import { Edit, StyledHeading, StyledMainWrapper } from '../components/styles/Docs';
 
