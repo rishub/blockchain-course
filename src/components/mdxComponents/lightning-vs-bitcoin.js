96a77e495392a5c6c772fc42af7d6bdde50fa1cf
Rishub Kumar <rishub@gmail.com>
2020-06-27 13:53:49 -0700

    counter

--- src/components/mdxComponents/lightning-vs-bitcoin.js
+++ src/components/mdxComponents/lightning-vs-bitcoin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Fragment } from 'react';
 
 import { makeStyles as useStyles } from '@material-ui/core/styles';
@@ -8,6 +8,17 @@ const lightningLogo = require('../images/lightning.png')
 const bitcoinLogo = require('../images/bitcoin.png')
 
 const LightningVsBitcoin = () => {
+    const [numBitcoinTxns, setBitcoinTxns] = useState(0);
+    const [numLightningTxns, setLightningTxns] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => setBitcoinTxns(numBitcoinTxns + 1), 200);
+    }, [numBitcoinTxns])
+
+    useEffect(() => {
+        setTimeout(() => setLightningTxns(numLightningTxns + 20), 20)
+    }, [numLightningTxns])
+
     const classes = useStyles({
         logo: {
             width: 150,
@@ -25,6 +36,8 @@ const LightningVsBitcoin = () => {
                 <div id="lightning">
                     <img className={classes.logo} src={lightningLogo} />
                 </div>
+                <p>Number of bitcoin txns: {numBitcoinTxns}</p>
+                <p>Number of lightning network txns: {numLightningTxns}</p>
             </div>
             <p style={{ textAlign: "center", marginTop: "10px" }}>The speed of the lightning network relative to bitcoin</p>
         </Fragment>
