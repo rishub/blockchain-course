4a07de29247eaa51f06b5df8c62ea665e25483c5
jamesylgan <jg893@cornell.edu>
2020-07-13 23:39:47 -0500

    reorganize sections, separate blockchain intro and course intro

--- src/components/mdxComponents/anchor.js
+++ src/components/mdxComponents/anchor.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
 const AnchorTag = ({ children: link, ...props }) => {
-  if (link) {
+  if (link && (props.target != "_self")) {
     return (
       <a href={props.href} target="_blank" rel="noopener noreferrer">
+        {link}
+      </a>
+    );
+  } else if (link) {
+    return (
+      <a href={props.href} target="_self" rel="noopener noreferrer">
         {link}
       </a>
     );
