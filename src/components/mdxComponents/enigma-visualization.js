908d242aa875d9364bbae107dc11a36781c0b617
Rishub Kumar <rishub@gmail.com>
2020-07-19 19:49:01 -0700

    clean

--- src/components/mdxComponents/enigma-visualization.js
+++ src/components/mdxComponents/enigma-visualization.js
@@ -41,14 +41,7 @@ const EnigmaVisualization = () => {
                     <div>{visual === "Public blockchain" && <span>&#8594;</span>}<a onClick={() => setVisual("Public blockchain")} style={{ cursor: "pointer", marginBottom: "5px" }}>Public blockchain</a></div>
                     <div>{visual === "Enigma" && <span>&#8594;</span>}<a onClick={() => setVisual("Enigma")} style={{ cursor: "pointer", marginBottom: "5px" }}>Enigma</a></div>
                 </div>
-                <div style={{ display: "flex" }} className={visual === "Public blockchain" ? "visible" : "hidden"}>
-                    {visual === "Public blockchain" && <Fragment>
-                    <Box>Blockchain</Box>
-                    <div style={{ alignSelf: "center", width: "100px", textAlign: "center" }}>Blockchain consists of a network of public data<br/><span style={{ fontSize: "36px" }}>&rarr;</span></div>
-                    <img src={publicBlockchain} style={{ width: "150px", height: "150px", alignSelf: "center", marginRight: "10px", marginLeft: "10px" }} />
-                    <div style={{ alignSelf: "center", width: "100px", textAlign: "center" }}>A User can easily access this public data with full transparency<br/><span style={{ fontSize: "36px" }}>&rarr;</span></div>
-                    <Box>User</Box></Fragment>}
-                </div>
+                
                 <div className={visual === "Enigma" ? "visible" : "hidden"}>
                     {visual === "Enigma" && <Fragment><div style={{ display: "flex" }}>
                         <Box style={{ background: "#90EE90" }}>Public to Private hash table data mapping</Box>
@@ -66,6 +59,15 @@ const EnigmaVisualization = () => {
                         <div style={{ fontSize: "30px", marginLeft: "-10px", marginTop: "-10px" }}>&uarr;</div>
                     </div>
                     <div style={{ marginLeft: "180px", padding: "5px 0 10px 0" }}>private blockchain data</div></Fragment>}
+                </div>
+
+                <div style={{ display: "flex" }} className={visual === "Public blockchain" ? "visible" : "hidden"}>
+                    {visual === "Public blockchain" && <Fragment>
+                    <Box>Blockchain</Box>
+                    <div style={{ alignSelf: "center", width: "100px", textAlign: "center" }}>Blockchain consists of a network of public data<br/><span style={{ fontSize: "36px" }}>&rarr;</span></div>
+                    <img src={publicBlockchain} style={{ width: "150px", height: "150px", alignSelf: "center", marginRight: "10px", marginLeft: "10px" }} />
+                    <div style={{ alignSelf: "center", width: "100px", textAlign: "center" }}>A User can easily access this public data with full transparency<br/><span style={{ fontSize: "36px" }}>&rarr;</span></div>
+                    <Box>User</Box></Fragment>}
                 </div>
             </div>
             <p style={{ textAlign: "center", marginTop: "10px" }}>The difference between data served by the public blockchain vs Enigma</p>
